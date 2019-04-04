60c374aca63575c88c3a8518a176bc6a578b8e1e
jawhite1612 <jawhite1612@gmail.com>
2019-04-03 23:02:43 -0400

    Started View Recipe Activity

---
+++ app/src/main/java/com/example/homepage/ViewRecipe.java
@@ -0,0 +1,54 @@
+package com.example.homepage;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ViewRecipe extends AppCompatActivity {
+
+    private Fragment viewRecipe;
+    private Fragment viewIngredients;
+    private FragmentTransaction transaction;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_ingredients:
+                    transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_container, viewIngredients);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                    return true;
+                case R.id.navigation_recipe:
+                    transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_container, viewRecipe);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_recipe);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        viewRecipe = new ViewRecipeFragment();
+        viewIngredients = new ViewIngredientsFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, viewIngredients).commit();
+    }
+
+}
