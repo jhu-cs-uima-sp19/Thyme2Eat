abe444bc6569953cbbe892282da625eb65937557
msethi3 <msethi3@jhu.edu>
2019-04-07 11:26:24 -0400

    "calendar to create meal plan files"

---
+++ app/src/main/java/com/example/homepage/CalendarCreateMealPlan.java
@@ -0,0 +1,14 @@
+package com.example.homepage;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CalendarCreateMealPlan extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_create_meal_plan);
+    }
+
+}
