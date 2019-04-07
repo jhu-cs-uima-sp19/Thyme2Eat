fe0470d934c1e14f41d6a3a874f5527d471ec657
EmreOzgu <s.emre_ozgu@hotmail.com>
2019-04-07 11:19:09 -0400

    added date variables

--- app/src/main/java/com/example/homepage/Meal.java
+++ app/src/main/java/com/example/homepage/Meal.java
@@ -3,6 +3,9 @@ package com.example.homepage;
 public class Meal {
     private String date;
     private String time;
+    private String dayName;
+    private String month;
+    private String dayNum;
 
     public Meal(String d, String t) {
         this.date = d;
