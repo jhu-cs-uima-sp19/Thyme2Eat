8678fd4b277561cc8af2c7c04871c634594b8c24
msethi3 <msethi3@jhu.edu>
2019-04-14 19:42:22 -0400

    added month on top w/ no errors

--- app/src/main/java/com/example/homepage/CreateMealPlan.java
+++ app/src/main/java/com/example/homepage/CreateMealPlan.java
@@ -9,6 +9,8 @@ import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -35,6 +37,7 @@ public class CreateMealPlan extends AppCompatActivity{
     private SharedPreferences.Editor editor;
     private ArrayList<Date> selectedDates = new ArrayList<>();
     List<Date> datesPicked;
+    TextView monthTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,15 +50,17 @@ public class CreateMealPlan extends AppCompatActivity{
         confirmBtn.setText("Confirm");
         planTextView = (TextView) findViewById(R.id.planTextView);
         planTextView.setText("Tap the dates to plan for!");
-        Calendar calendarDate = Calendar.getInstance();
-        String currentDate = DateFormat.getDateInstance().format(calendarDate.getTime());
-        String currDate = currentDate.substring(4,6);
-        cDate = Integer.parseInt(currDate);
         datesPicked = new ArrayList<>();
         myPreferences = getSharedPreferences("preferences", MODE_PRIVATE);
         editor = myPreferences.edit();
         final Date todayDate = new Date();
-
+        monthTV = (TextView) findViewById(R.id.monthTV);
+        Calendar c = Calendar.getInstance();
+        int y = c.get(Calendar.YEAR);
+        int m = c.get(Calendar.MONTH);
+        String month = convertMonth(m);
+        monthTV.setText(month + " " + y);
+        //monthTV.setTextColor(Color.RED);
 
         confirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -160,6 +165,51 @@ public class CreateMealPlan extends AppCompatActivity{
         }
         convertedDate += date.substring(8,10);
         return convertedDate;
+    }
+
+    public String convertMonth(int m){
+        m += 1;
+        String month = "";
+        switch(m){
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+        }
+        return month;
+
     }
 
 }
